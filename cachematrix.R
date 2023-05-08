1970869a2620bfbff029d5e7d51b4205d8c7f6c0
ian gingerich <gingerii@ohsu.edu>
2023-05-08 16:27:05 -0700

    added code to starter file functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## function to create a special matrix that chaches its inverse 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        # set the value of the matrix 
+        set <- function(y){
+                x <<-y
+                m <<- NULL
+        }
+        #gets val of matrix 
+        get <- function() x
+        setInv <- function(Inv) m <<- Inv
+        getInv <-  function() m
+        list(set=set,get=get,
+             setInv = setInv,
+             getInv = getInv)
+        
 }
 
 
-## Write a short comment describing this function
-
+## retrieve this cached matrix, or calculate inverse 
 cacheSolve <- function(x, ...) {
+        m <- x$getInv()
+        if(!is.null(m)){
+                message('getting cached matrix')
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data,...)
+        x$setInv(m)
+        m
         ## Return a matrix that is the inverse of 'x'
 }
